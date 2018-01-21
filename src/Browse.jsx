60e684949fd287c2b7b800d3057607de95bddd5b
Enrique <lukas100519@outlook.com>
2018-01-21 04:37:22 +0100

    Musicer V1.4.5 - fix some errors

--- src/Browse.jsx
+++ src/Browse.jsx
@@ -5,46 +5,49 @@ Link,
 IndexLink,
 IndexRoute,
 hashHistory,
-withRouter        
+withRouter
 } from 'react-router';
 import Login from './Login.jsx';
 
-class Browse extends React.Component { 
-    
-    
+class Browse extends React.Component {
+
+
     componentDidMount(){
-        
-        
+
+
         // pobranie wartosci
         const div = $('#app');
-        
+        const bgvideo = $('source');
+
         const getSession = sessionStorage.getItem('logged');
         const sessionValue = $.parseJSON(getSession);
-        
+
         // ----------------------------------------------------
 
         if (getSession != null) {
-          
+
             div.addClass('gradient');
-            
+            bgvideo.remove();
+
           }
     }
-         
-    
+
+
     Logout = () => {
-        
+
         const div = $('#app');
-        
+
         sessionStorage.removeItem('logged');
-        
+
         this.props.router.push('/login');
-        
-        div.removeClass('gradient'); 
-        
+
+        div.removeClass('gradient');
+
+
     }
-        
+
 	render(){
-            
+
             return <div className="container">
                         <div className="row">
                             <div className='rightbar'>
@@ -52,22 +55,24 @@ class Browse extends React.Component {
                                     <Link to='/browse'><img src='./images/logo-browse.png' /></Link>
                                 </div>
                                 <div className='menu-right'></div>
+                            <div  className='Settings'>
                                 <div className='right-account'>
                                     <Link to='/browse/account'><img src='./images/circle.jpg' /></Link>
                                 </div>
                                     <Link to="/" onClick={ this.Logout } className="logout">Log Out</Link>
                             </div>
+                          </div>
                             <div className='leftbar'>
-                                   
+
                             </div>
-            
+
                                 {this.props.children}
-                            
+
                         </div>
                     </div>
 
-}	
+}
 
 }
-         
+
 module.exports = withRouter(Browse);
